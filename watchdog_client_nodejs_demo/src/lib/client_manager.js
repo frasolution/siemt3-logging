5027dff173f4e62fd8eecb70e2ee3278f1687c6f
Max M <meshstyles@users.noreply.github.com>
2021-01-12 16:11:46 +0100

    fixed unsave pactices

--- watchdog_client_nodejs_demo/src/lib/client_manager.js
+++ watchdog_client_nodejs_demo/src/lib/client_manager.js
@@ -1,11 +1,24 @@
+/*++
+Project Name:
+watchdog_client
+
+Author:
+Maximilian Medlin (Meshstyles)
+
+Description:
+This manager handles having a currently valid jwt token and mitigatest the need to request a new token each request
+
+--*/
+
 const auth_module = require('./auth');
 
 class PrivateSingleton {
     constructor() {
+        // dummy data -- 0 is most likely before the current time
         this.jwtoken = { jwt: "lol", timeframe: 0 };
-        this.firstread = false;
     }
 }
+
 class Singleton {
     constructor() {
         throw new Error('Use Singleton.getInstance()');
@@ -13,15 +26,11 @@ class Singleton {
     static getInstance = async () => {
         if (!Singleton.instance) {
             Singleton.instance = new PrivateSingleton();
-            // console.log(Singleton.instance);
-            if (! (await auth_module.isValidJwt(Singleton.instance.jwtoken)) ) {
+        }
+        if (! (await auth_module.isValidJwt(Singleton.instance.jwtoken)) ) {
                 Singleton.instance.jwtoken = await auth_module.login();
-            }
         }
         return Singleton.instance;
-    }
-    set firstread(state) {
-        this.instance.firstread = state;
     }
 }
 module.exports = Singleton;
