eb3bf31571d918f580c9e82a1fbee594ea1f586d
Max M <meshstyles@users.noreply.github.com>
2021-03-13 03:20:12 +0100

    minor refactor condb.app

--- watchdog_server/src/main/java/com/siemt3/watchdog_server/condb/App.java
+++ watchdog_server/src/main/java/com/siemt3/watchdog_server/condb/App.java
@@ -8,7 +8,10 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
+    public static void dbCommit(String[] args) throws SQLException {
+        //TODO make sessions
+        //TODO make prefab statement
+        //TODO make proper deconstruct function
         String url = "jdbc:mysql://localhost:3306/SIEM?createDatabaseIfNotExist=true&serverTimezone=UTC";
         String user = "root";
         String password = "Asdfasdf6634";
@@ -16,6 +19,7 @@ public class App {
             Connection myConn = DriverManager.getConnection(url, user, password);
             Statement myStatement = myConn.createStatement();
             long timestamp = current_time();
+            //errors like alerts can and column names can be ignored here because intelij does not read the data source properly
             myStatement.executeUpdate("insert into alerts (event_id, event_type, event_name, priority, custom_data,date) values ('TEST', 'TEST','TEST', 1 , 'TEST', FROM_UNIXTIME("+ timestamp +") )");
         }catch (SQLException e){
             e.printStackTrace();
