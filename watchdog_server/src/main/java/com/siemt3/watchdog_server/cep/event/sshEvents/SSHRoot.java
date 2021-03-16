e127c3ac2ad4b29f5423d3d7daec0cfae63e569b
Max M <meshstyles@users.noreply.github.com>
2021-03-16 02:38:30 +0100

    Update SSHRoot.java

--- watchdog_server/src/main/java/com/siemt3/watchdog_server/cep/event/sshEvents/SSHRoot.java
+++ watchdog_server/src/main/java/com/siemt3/watchdog_server/cep/event/sshEvents/SSHRoot.java
@@ -4,21 +4,15 @@ import com.siemt3.watchdog_server.EventId;
 import com.siemt3.watchdog_server.EventType;
 
 public class SSHRoot {
-    private String eventId;
-    private String eventType;
     private long arrival_time;
     private String ip;
 
     public SSHRoot(long arrival_time, String ip) {
-        this.eventId = EventId.SSH_Root;
-        this.eventType  = EventType.SSH_Root;
         this.arrival_time = arrival_time;
         this.ip = ip;
     }
 
     public SSHRoot(){
-        this.eventId = EventId.SSH_Root;
-        this.eventType  = EventType.SSH_Root;
     }
 
     public long getArrival_time() {
