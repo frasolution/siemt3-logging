c411387adab3b9ef394b64096a24cb304f96dd94
Max M <meshstyles@users.noreply.github.com>
2021-03-17 21:51:49 +0100

    adjusted static variables

--- watchdog_server/src/main/java/com/siemt3/watchdog_server/EventType.java
+++ watchdog_server/src/main/java/com/siemt3/watchdog_server/EventType.java
@@ -1,14 +1,7 @@
 package com.siemt3.watchdog_server;
 
 public class EventType {
-    // RULE KAT+EVENT_NUMBER
-    // KAT SQL INJECT = 4 | EVENT_NUMBER 1;
-    // => 41
-
-    // SSH = KAT 3
-    public static final String SSH_Dictionary = "SSH_Failed_Password";
-    public static final String SSH_Root = "SSH_Root";
-    public static final String SSH_Algorithm = "SSH_Algorithm";
-    public static final String SSH_User = "SSH_User";
-    public static final String SSH_Ip = "SSH_Ip";
+    public static final String APACHE2 = "apache2 webserver";
+    public static final String PORT_SCAN = "port scan";
+    public static final String SSH = "ssh";
 }
