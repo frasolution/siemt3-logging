69b5e847fec65a1a526fbf4b3e087d7c67093833
HeidenSpatz <uwelitschel@gmail.com>
2021-03-23 18:29:37 +0100

    Delete watchdog_server directory

--- watchdog_server/src/main/java/com/siemt3/watchdog_server/Severity.java
+++
@@ -1,7 +0,0 @@
-package com.siemt3.watchdog_server;
-
-public class Severity {
-    public static final int GREEN = 1;
-    public static final int YELLOW = 2;
-    public static final int RED = 3;
-}
