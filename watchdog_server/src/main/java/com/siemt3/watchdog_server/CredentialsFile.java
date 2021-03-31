7a5a5cb14a4d8f4b22d285dc7f1f384e9f298a60
Max M <meshstyles@users.noreply.github.com>
2021-04-01 01:14:11 +0200

    put credentials in extra file to be excluded if prod credentails were to be used

---
+++ watchdog_server/src/main/java/com/siemt3/watchdog_server/CredentialsFile.java
@@ -0,0 +1,7 @@
+package com.siemt3.watchdog_server;
+
+public class CredentialsFile {
+    public static final String URL = "jdbc:mysql://localhost:3306/SIEM?createDatabaseIfNotExist=true&serverTimezone=UTC";
+    public static final String USER = "root";
+    public static final String PASSWORD = "Asdfasdf6634";
+}
