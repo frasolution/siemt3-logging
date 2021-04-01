c53b174b541113b635385bb6d971391c4396d217
Max M <meshstyles@users.noreply.github.com>
2021-04-01 03:22:00 +0200

    added comments to static variable files

--- watchdog_server/src/main/java/com/siemt3/watchdog_server/CredentialsFile.java
+++ watchdog_server/src/main/java/com/siemt3/watchdog_server/CredentialsFile.java
@@ -1,6 +1,9 @@
 package com.siemt3.watchdog_server;
 
 public class CredentialsFile {
+    //-------ATTENTION----------
+    // PLEASE EXCLUDE THIS FILE IN AS SOON AS YOU DON'T USE DEV CREDENTIALS FOR LOCAL MACHINES!
+    // IF YOU TEST ON A PROD OR TEST SERVER OR IN CLOUD DO NOT PUSH THIS FILE TO GIT!
     public static final String URL = "jdbc:mysql://localhost:3306/SIEM?createDatabaseIfNotExist=true&serverTimezone=UTC";
     public static final String USER = "root";
     public static final String PASSWORD = "Asdfasdf6634";
--- watchdog_server/src/main/java/com/siemt3/watchdog_server/GlobalVariables.java
+++ watchdog_server/src/main/java/com/siemt3/watchdog_server/GlobalVariables.java
@@ -1,6 +1,7 @@
 package com.siemt3.watchdog_server;
 
 public class GlobalVariables {
+// Please use a slower update cycle in prod
 //    public static final long DBUPDATECYCLE = 3600000;
     public static final long DBUPDATECYCLE = 60000;
     public static final boolean DEBUG_FLAG = true;
