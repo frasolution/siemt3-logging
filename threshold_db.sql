a1aaff1ccdb07592c1e89e812393e2e09b28dd67
Max M <meshstyles@users.noreply.github.com>
2021-04-14 20:47:41 +0200

    Update threshold_db.sql

--- threshold_db.sql
+++ threshold_db.sql
@@ -5,3 +5,15 @@ CREATE TABLE `siem`.`thresholds` (
   `number` INT NOT NULL,
   PRIMARY KEY (`id`));
 SELECT * FROM `siem`.`thresholds`;
+
+insert into `siem`.`thresholds` (name, type, number) values ('test_count', 'test', 10);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_dictionary_basic_count', 'ssh', 5);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_dictionary_basic_time', 'ssh', 5);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_dictionary_elevated_count', 'ssh', 2);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_dictionary_elevated_time', 'ssh', 2);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_root_elevated_count', 'ssh', 2);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_root_elevated_time', 'ssh', 1);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_user_elevated_count', 'ssh', 2);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_user_elevated_time', 'ssh', 1);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_ip_elevated_count', 'ssh', 2);
+insert into `siem`.`thresholds` (name, type, number) values ('ssh_ip_elevated_time', 'ssh', 60);
